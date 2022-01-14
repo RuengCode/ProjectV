5098f1096b9dde79d97d6c75bb7621e9eac24469
NuiNui <kanyaratchaemsaing@gmail.com>
2022-01-15 01:18:17 +0700

    -fix : Change Color all Activity and Fragment

--- app/src/main/java/com/example/projectend/CustomDialogFragment.kt
+++ app/src/main/java/com/example/projectend/CustomDialogFragment.kt
@@ -1,14 +1,43 @@
 package com.example.projectend
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.fragment.app.DialogFragment
+import com.google.firebase.auth.FirebaseAuth
 
 class CustomDialogFragment : DialogFragment() {
-    override fun onCreateView(inflater : LayoutInflater,container : ViewGroup?,savedInstanceState : Bundle?): View {
-        var view = inflater.inflate(R.layout.fragment_custom_dialog,container,false)
-        return view
+    private lateinit var ratingRadioGroup:RadioGroup
+    private lateinit var submitButton:Button
+
+    override fun onCreateView(
+        inflater : LayoutInflater,
+        container : ViewGroup?,
+        savedInstanceState : Bundle?
+    ): View {
+
+        var rootView = inflater.inflate(R.layout.fragment_custom_dialog,container,false)
+//        ratingRadioGroup = rootView.findViewById(R.id.ratingRadioGroup)
+        submitButton = rootView.findViewById(R.id.submitButton)
+        submitButton.setOnClickListener{
+            ratingRadioGroup = rootView.findViewById(R.id.ratingRadioGroup1)
+
+            ratingRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+                if (checkedId == R.id.excellentRadioButton1 ){
+                    Toast.makeText(context,"yes",Toast.LENGTH_LONG).show()
+                }
+                if (checkedId == R.id.excellentRadioButton2){
+                    Toast.makeText(context,"no",Toast.LENGTH_LONG).show()
+                }
+
+            }
+
+
+
+        }
+        return rootView
     }
 }
--- app/src/main/java/com/example/projectend/activity/MainActivity.kt
+++ app/src/main/java/com/example/projectend/activity/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.projectend.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projectend.CustomDialogFragment
 import com.example.projectend.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rateMeTextView.setOnClickListener{
+            var dialog = CustomDialogFragment()
+
+            dialog.show(supportFragmentManager,"customDialog")
+        }
     }
 
 }
